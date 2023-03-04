57b7eae145455d488e7a275962c366fd7730529e
MatheusZuchiBalbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-03-03 22:15:32 -0300

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,12 @@ function Header() {
         <div className={styles.main_header} >
             <div className={styles.poke_icon}>
                 <img src="https://cdn-icons-png.flaticon.com/512/1169/1169608.png" />
-                {/* AQUI VAI TER UM GIF DE POKEBOLA ABRINDO */}
             </div>
             <div  className={styles.header_input}>
                 <input type="text" placeholder="Digite o pokemon a ser pesquisado: "></input>
                 <button type="submit"> <BsSearch /> </button>
             </div>
             <div className={styles.logout_icon}>
-                <FiLogOut  />
             </div>
         </div>
     )
--- src/components/Poke_Card.jsx
+++ src/components/Poke_Card.jsx
@@ -6,39 +6,41 @@ function Poke_Card() {
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    // fetch('https://pokeapi.co/api/v2/pokemon/2/%27').then((res) => res.json()).then((data) => console.log(data))
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=5%27%27').then((res) => res.json()).then((data) => setList(data.results))
+    // fetch('https://pokeapi.co/api/v2/pokemon/4/').then((res) => res.json()).then((data) => console.log(data))
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=300%27%27%27').then((res) => res.json()).then((data) => setList(data.results))
   }, []);
 
     return (
       <div style={{display: 'flex', flex: 'none', flexWrap:'wrap'}}>
-          { list.map((item) => <> <EachPokemon pokemon={item}/> </>)}
+          { list.map((item) => <> <EachPokemon pokemon={item} /> </>)}
       </div>
     );
 }
 
   const EachPokemon = ( {pokemon} ) => {
-
     const [image, setImage] = useState(['']);
     const [type, setType] = useState(['']);
 
-    useEffect(() => {
-      // console.log(pokemon)
+    useEffect((e) => {
+      // console.log(pokemon);
       fetch(pokemon.url).then((res) => res.json()).then((one_pokemon_url) => setImage(one_pokemon_url.sprites));
       fetch(pokemon.url).then((res) => res.json()).then((one_pokemon_url) => setType(one_pokemon_url.types));
     }, [])
 
-    return (
-      <div className={styles.PokeCards}>
-        <div className={styles.Card}>
-        <img src={image.front_default} style={{width: 'auto', height: 'auto'}} alt={pokemon.name}></img>
-        <div className="values">
-          <h4 className={styles.card_title}> {pokemon.name} </h4>
-          {/* <> {type[1] ? <p> {type[0].type.name} {type[1].type.name} </p> : <p> {type[0].type.name} </p>} </> */}
+    if (type[0]) {
+      return (
+        <div className={styles.PokeCards}>
+          <div className={styles.Card}>
+          <img src={image.front_default} style={{width: '100px', height: 'auto'}} alt={pokemon.name}></img>
+          <div className="values">
+            <h4 className={styles.card_title}> {pokemon.name} </h4>
+            {type[1] ? <p> {type[0].type.name} <br></br> {type[1].type.name} </p> : <p> {type[0].type.name} </p>}
+          </div>
+          </div>
         </div>
-        </div>
-      </div>
-    )
+      )
+    }
+
   }
 
 export default Poke_Card;
