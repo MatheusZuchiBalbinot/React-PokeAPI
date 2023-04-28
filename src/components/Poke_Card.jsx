612576b41a90a71e19c0bd1a44b111ac41f945e5
Matheus Zuchi Balbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2023-04-27 21:20:29 -0300

    Update Poke_Card.jsx

--- src/components/Poke_Card.jsx
+++ src/components/Poke_Card.jsx
@@ -8,27 +8,16 @@ import SearchPokemon from './search_Pokemon';
 function Poke_Card() {
   const [list, setList] = useState([]);
 
-  // function checkNewPokemons(newPokemon) {
-  //   console.log(newPokemon);
-  //   if(newPokemon) {
-  //     console.log('a')
-  //   }
-  // }
-
   useEffect(() => {
-    // fetch('https://pokeapi.co/api/v2/pokemon/4/').then((res) => res.json()).then((data) => console.log(data)) 
     fetch('https://pokeapi.co/api/v2/pokemon?limit=100%27%27%27').then((res) => res.json()).then((data) => setList(data.results));
   }, []);
 
     return (
-      // <>{console.log(list)} 
       <div className={styles.PokeCardDiv}>
         <div className={styles.CardsDiv}>
-            {/* {checkNewPokemons()} */}
             { list.map((item) => <> <EachPokemon pokemon={item} /> <SearchPokemon pokemon={item}/> </>)}
         </div>
       </div>
-      // </>
     );
 
 }
